5fc0e4b49ce3ebf6fcc952c609df70ec1daf6630
EPostanogova <e_postanogova@inbox.ru>
2023-03-24 13:02:03 +0300

    test

---
+++ Test/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+{
+    public GameObject obj;
+    Vector3 cord;
+    void Start()
+    {
+        cord = obj.transform.position+ new Vector3(0,0.3f,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            transform.position = cord;
+        }
+    }
+}
---
+++ Test/Assets/trigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject body;
+    public Camera cam;
+    public Camera cam0;
+    private void Start()
+    {
+        anim =GameObject.Find("Camera").GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("play"))
+        {
+            body.SetActive(true);
+            anim.SetBool("Start",true);
+            cam0.enabled = false;
+            cam.enabled = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.Equals("play"))
+        {
+            cam0.enabled = true;
+            cam.enabled = false;
+        }
+    }
+}
